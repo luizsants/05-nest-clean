0b1669aea6cb1accdb2f215b749614f69298e248
Luiz Fernando dos Santos Carvalho <luizfernando.santos900@gmail.com>
2025-11-12 13:35:06 -0300

    fix commonJS error

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
-import 'reflect-metadata' 
+import 'reflect-metadata'
 import 'dotenv/config'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module.js'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
