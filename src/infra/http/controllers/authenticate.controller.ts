470ab2c7f593fd688c75989c17ba0be3649349a7
luizsants <luizfernando.santos900@gmail.com>
2025-12-29 15:23:00 -0300

    working infra folder & prisma service

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -7,7 +7,7 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import z from 'zod'
 import { compare } from 'bcryptjs' // Replace with real hash comparison
 
