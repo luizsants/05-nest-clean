163948e68e8d76da3e1250698c4b3ba980625d04
luizsants <luizfernando.santos900@gmail.com>
2025-12-30 09:41:39 -0300

    prisma repository

---
+++ src/infra/database/prisma/repositories/prisma-questions-comments-repository.ts
@@ -0,0 +1,24 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comments'
+import { Injectable } from '@nestjs/common'
+
+@Injectable()
+export class PrismaQuestionCommentsRepository implements QuestionCommentsRepository {
+  findById(id: string): Promise<QuestionComment | null> {
+    throw new Error('Method not implemented.')
+  }
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams,
+  ): Promise<QuestionComment[]> {
+    throw new Error('Method not implemented.')
+  }
+  create(question: QuestionComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+  delete(question: QuestionComment): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+}
