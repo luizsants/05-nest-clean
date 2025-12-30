614b89b46e39af12f17b814bb93d03f54e72b4b0
luizsants <luizfernando.santos900@gmail.com>
2025-12-30 14:52:30 -0300

    test env schema chenged to public, git cherry pick worked fine

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
-import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
-import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
-import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
-import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
+import { PrismaQuestionRepository } from './prisma/repositories/prisma-questions-repository'
+import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-questions-comments-repository'
+import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-questions-attachments-repository'
+import { PrismaAnswerRepository } from './prisma/repositories/prisma-answer-repository'
 import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -13,11 +13,11 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     PrismaService,
     {
       provide: QuestionsRepository,
-      useClass: PrismaQuestionsRepository,
+      useClass: PrismaQuestionRepository,
     },
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
-    PrismaAnswersRepository,
+    PrismaAnswerRepository,
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentsRepository,
   ],
@@ -26,7 +26,7 @@ import { QuestionsRepository } from '@/domain/forum/application/repositories/que
     QuestionsRepository,
     PrismaQuestionCommentsRepository,
     PrismaQuestionAttachmentsRepository,
-    PrismaAnswersRepository,
+    PrismaAnswerRepository,
     PrismaAnswerCommentsRepository,
     PrismaAnswerAttachmentsRepository,
   ],
