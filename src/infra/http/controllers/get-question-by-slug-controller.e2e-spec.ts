9e41fc9bd7d361b8f5f0e7108a54439f13f5750c
F6484175 Luiz Fernando Dos Santos Carvalho <luiz.santosc@bb.com.br>
2026-02-09 14:04:41 -0300

    student & question factories

--- src/infra/http/controllers/get-question-by-slug-controller.e2e-spec.ts
+++ src/infra/http/controllers/get-question-by-slug-controller.e2e-spec.ts
@@ -1,22 +1,28 @@
+import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { DatabaseModule } from '@/infra/database/database.module'
 import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
 
 describe('Get question by slug Controller (e2e)', () => {
   let app: INestApplication
-  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
-    prisma = moduleRef.get<PrismaService>(PrismaService)
+    studentFactory = moduleRef.get<StudentFactory>(StudentFactory)
+    questionFactory = moduleRef.get<QuestionFactory>(QuestionFactory)
     jwt = moduleRef.get<JwtService>(JwtService)
 
     await app.init()
@@ -29,25 +35,15 @@ describe('Get question by slug Controller (e2e)', () => {
   test('[GET] /questions/:slug - should get question by slug', async () => {
     const email = 'fetch-user@example.com'
 
-    const user = await prisma.user.create({
-      data: {
-        name: 'Fetch User',
-        email,
-        password: 'password123',
-      },
-    })
+    const user = await studentFactory.makePrismaStudent({ email })
 
-    // Cria 2 perguntas para este usuário
-    await prisma.question.create({
-      data: {
-        title: 'Question 1',
-        slug: 'question-1-slug',
-        content: 'Content for question 1',
-        authorId: user.id,
-      },
-    })
+    const accessToken = jwt.sign({ sub: user.id.toString() })
 
-    const accessToken = jwt.sign({ sub: user.id })
+    await questionFactory.makePrismaQuestion({
+      authorId: user.id,
+      title: 'Question 1',
+      slug: Slug.create('question-1-slug'),
+    })
 
     // Busca as perguntas do usuário
     const response = await request(app.getHttpServer())
